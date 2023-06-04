60365657d963f56cd524127190a3b6173953d4bf
André Baldeweg <a@baldeweg.org>
2023-06-04 19:06:53 +0000

    Index is a param

--- search-api/web/controller.go
+++ search-api/web/controller.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Search(c *gin.Context) {
-	status, data := api.NewRequest("POST", "/indexes/books/search", c.Request.Body)
+	status, data := api.NewRequest("POST", "/indexes/"+c.Param("index")+"/search", c.Request.Body)
 
 	c.JSON(status, data)
 }
@@ -30,25 +30,25 @@ func Remove(c *gin.Context) {
 }
 
 func RemoveDocuments(c *gin.Context) {
-	status, data := api.NewRequest("DELETE", "/indexes/books/documents", c.Request.Body)
+	status, data := api.NewRequest("DELETE", "/indexes/"+c.Param("index")+"/documents", c.Request.Body)
 
 	c.JSON(status, data)
 }
 
 func CreateDocument(c *gin.Context) {
-	status, data := api.NewRequest("POST", "/indexes/books/documents", c.Request.Body)
+	status, data := api.NewRequest("POST", "/indexes/"+c.Param("index")+"/documents", c.Request.Body)
 
 	c.JSON(status, data)
 }
 
 func GetSettings(c *gin.Context) {
-	status, data := api.NewRequest("GET", "/indexes/books/settings", c.Request.Body)
+	status, data := api.NewRequest("GET", "/indexes/"+c.Param("index")+"/settings", c.Request.Body)
 
 	c.JSON(status, data)
 }
 
 func UpdateSettings(c *gin.Context) {
-	status, data := api.NewRequest("PATCH", "/indexes/books/settings", c.Request.Body)
+	status, data := api.NewRequest("PATCH", "/indexes/"+c.Param("index")+"/settings", c.Request.Body)
 
 	c.JSON(status, data)
 }
