06f41d29a0c6ed5c60896f03539f3d048256c783
André Baldeweg <a@baldeweg.org>
2023-06-17 10:26:33 +0000

    Add test

--- search-api/validation/validation_test.go
+++ search-api/validation/validation_test.go
@@ -1,6 +1,12 @@
 package validation
 
-import "testing"
+import (
+	"testing"
+)
+
+type Product struct {
+	Name string `json:"name" validate:"required"`
+}
 
 func TestVar(t *testing.T) {
 	type args struct {
@@ -33,6 +39,30 @@ func TestVar(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := Var(tt.args.name, tt.args.constraints); (err != nil) != tt.wantErr {
 				t.Errorf("Var() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestStruct(t *testing.T) {
+	type args struct {
+		s interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"test product",
+			args{Product{"test"}},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Struct(tt.args.s); (err != nil) != tt.wantErr {
+				t.Errorf("Struct() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
