cc9b3b0d36206f931439209f8abe3737b33a4694
André Baldeweg <a@baldeweg.org>
2023-06-24 18:24:38 +0000

    Fix tests

--- search-api/update/update_test.go
+++ search-api/update/update_test.go
@@ -8,35 +8,38 @@ import (
 )
 
 type MockClient struct {
-    IndexFunc func(uid string) *meilisearch.Index
-	GetIndexesFunc func(*meilisearch.IndexesQuery) (*meilisearch.IndexesResults, error)
-    CreateIndexFunc func(config *meilisearch.IndexConfig) (resp *meilisearch.TaskInfo, err error)
-    DeleteIndexFunc func(uid string) (resp *meilisearch.TaskInfo, err error)
+	IndexFunc       func(uid string) *meilisearch.Index
+	GetIndexesFunc  func(*meilisearch.IndexesQuery) (*meilisearch.IndexesResults, error)
+	CreateIndexFunc func(config *meilisearch.IndexConfig) (resp *meilisearch.TaskInfo, err error)
+	DeleteIndexFunc func(uid string) (resp *meilisearch.TaskInfo, err error)
 }
 
 func (m *MockClient) Index(uid string) *meilisearch.Index {
-    return nil
+	return m.IndexFunc(uid)
 }
 
-func (m *MockClient) GetIndexes(*meilisearch.IndexesQuery) (*meilisearch.IndexesResults, error) {
-    indexes := &meilisearch.IndexesResults{
-        Results: []meilisearch.Index{{UID:"products_1"}},
-        Offset: 0,
-        Limit: 0,
-        Total:1,
-    }
-	return indexes,nil
+func (m *MockClient) GetIndexes(indexes *meilisearch.IndexesQuery) (*meilisearch.IndexesResults, error) {
+	return m.GetIndexesFunc(indexes)
 }
 
 func (m *MockClient) CreateIndex(config *meilisearch.IndexConfig) (resp *meilisearch.TaskInfo, err error) {
-    return nil,nil
+	return m.CreateIndexFunc(config)
 }
 func (m *MockClient) DeleteIndex(uid string) (resp *meilisearch.TaskInfo, err error) {
-    return nil, nil
+	return m.DeleteIndexFunc(uid)
 }
 
 func TestUpdateConf_getIndexes(t *testing.T) {
-    client = &MockClient{}
+	client = &MockClient{GetIndexesFunc: func(*meilisearch.IndexesQuery) (*meilisearch.IndexesResults, error) {
+		indexes := &meilisearch.IndexesResults{
+			Results: []meilisearch.Index{{UID: "products_1"}},
+			Offset:  0,
+			Limit:   0,
+			Total:   1,
+		}
+		return indexes, nil
+	}}
+
 	type fields struct {
 		AllowedBranches []string
 		AllowedIndexes  []string
@@ -48,7 +51,7 @@ func TestUpdateConf_getIndexes(t *testing.T) {
 	tests := []struct {
 		name   string
 		fields fields
-        want   []string
+		want   []string
 	}{
 		{
 			"Indexes should exist",
@@ -70,7 +73,7 @@ func TestUpdateConf_getIndexes(t *testing.T) {
 				MustRemove:      tt.fields.MustRemove,
 			}
 			conf.getIndexes()
-            if got := conf.Indexes; !reflect.DeepEqual(got, tt.want) {
+			if got := conf.Indexes; !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("conf.Indexes = %v, want %v", got, tt.want)
 			}
 		})
@@ -136,9 +139,9 @@ func TestUpdateConf_getMustCreate(t *testing.T) {
 			"Indexes should exist",
 			fields{
 				AllowedIndexes:  []string{"products"},
-				AllowedBranches: []string{"1","3"},
-                Indexes: []string{"products_1"},
-                ShouldExist: []string{"products_1", "products_3"},
+				AllowedBranches: []string{"1", "3"},
+				Indexes:         []string{"products_1"},
+				ShouldExist:     []string{"products_1", "products_3"},
 			},
 			[]string{"products_3"},
 		},
@@ -179,9 +182,9 @@ func TestUpdateConf_getMustRemove(t *testing.T) {
 			"Indexes should exist",
 			fields{
 				AllowedIndexes:  []string{"products"},
-				AllowedBranches: []string{"1","2"},
-                Indexes: []string{"products_1", "products_3"},
-                ShouldExist: []string{"products_1"},
+				AllowedBranches: []string{"1", "2"},
+				Indexes:         []string{"products_1", "products_3"},
+				ShouldExist:     []string{"products_1"},
 			},
 			[]string{"products_3"},
 		},
