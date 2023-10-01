2ee68f0590f663cf6e25da2e8ef7f280b455e8ae
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2023-10-01 14:33:46 +0000

    Fix stats

--- admincli/cmd/stats.go
+++ admincli/cmd/stats.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/bcicen/go-units"
 	"github.com/spf13/cobra"
@@ -15,34 +16,49 @@ var statsCmd = &cobra.Command{
 	Short: "Show stats",
 	Long:  `Show stats`,
 	Run: func(cmd *cobra.Command, args []string) {
-		files, err := os.ReadDir(viper.GetString("stats_dir"))
-		if err != nil {
-			log.Fatalln(err)
-			return
-		}
+        fmt.Println("Directory size")
 
-		var totalSize int64
-		for _, file := range files {
-			if !file.IsDir() {
-				fileinfo, _ := file.Info()
-				totalSize += fileinfo.Size()
-			}
-		}
+		totalSize, err := getDirSize(viper.GetString("stats_dir"))
+        if err != nil {
+            log.Fatalln(err)
+        }
 
-		size := units.NewValue(float64(totalSize), units.Byte)
-		convertedSize := size.MustConvert(units.Gibibyte)
-		opts := units.FmtOptions{
-			Label:     true,
-			Short:     true,
-			Precision: 2,
-		}
-
-		fmt.Println("Directory size")
 		fmt.Println("Path", viper.GetString("stats_dir"))
-		fmt.Println("Size", convertedSize.Fmt(opts))
+		fmt.Println("Size", convertSizeToGb(totalSize))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(statsCmd)
+}
+
+func getDirSize(path string) (int64, error) {
+	var size int64
+
+	err := filepath.Walk(path, func(_ string, fileInfo os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !fileInfo.IsDir() {
+			size += fileInfo.Size()
+		}
+
+		return err
+	})
+
+	return size, err
+}
+
+func convertSizeToGb(data int64) string {
+	v := units.NewValue(float64(data), units.Byte)
+	c := v.MustConvert(units.Gibibyte)
+
+	opts := units.FmtOptions{
+		Label:     true,
+		Short:     true,
+		Precision: 2,
+	}
+
+	return c.Fmt(opts)
 }
