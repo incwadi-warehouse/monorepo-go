a3b6c46abb8e6a967b3b0fef95401bcb13a6bac0
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-05-21 16:34:22 +0000

    Add dump command

---
+++ admincli/cmd/dump.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var service = "bw-db-1"
+
+var dumpCmd = &cobra.Command{
+	Use:   "dump",
+	Short: "Dump database",
+	Long:  `Dumps the database to the file system`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Dumping database")
+
+		// command
+		c := exec.Command("docker", "exec", service, "sh", "-c", fmt.Sprintf("exec mysqldump %s -uroot -p\"%s\"", "$MYSQL_DATABASE", "$MYSQL_ROOT_PASSWORD"))
+
+		// create directory
+		dirPath := fmt.Sprintf("%s/dump/", viper.GetString("project_dir"))
+
+		if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+			err := os.MkdirAll(dirPath, 0755)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+
+		// create dump
+		weekday := strings.ToLower(time.Now().Weekday().String())
+
+		file, err := os.Create(fmt.Sprintf("%sdump_%s.sql", dirPath, weekday))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		// run command
+		c.Stdout = file
+		err = c.Run()
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("Done")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dumpCmd)
+}
