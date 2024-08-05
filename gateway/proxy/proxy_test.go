a9a46042a47ebac74848240ecb91563700738643
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-08-05 14:54:05 +0000

    Add test

---
+++ gateway/proxy/proxy_test.go
@@ -0,0 +1,47 @@
+package proxy
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProxy(t *testing.T) {
+    mockService := httptest.NewServer(
+        http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            w.WriteHeader(http.StatusOK)
+            _, _ = w.Write([]byte("TEST"))
+        }),
+    )
+    defer mockService.Close()
+
+    gin.SetMode(gin.TestMode)
+
+    router := gin.New()
+    router.GET("/test", func(c *gin.Context) {
+        err := Proxy(c, mockService.URL, "/test")
+        if err != nil {
+            t.Fatal(err)
+        }
+    })
+
+    req, err := http.NewRequest(http.MethodGet, "/test", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    w := httptest.NewRecorder()
+    router.ServeHTTP(w, req)
+
+    assert.Equal(t, http.StatusOK, w.Code)
+    
+    body, err := io.ReadAll(w.Body)
+    if err != nil {
+        t.Fatal(err)
+    }
+    assert.Equal(t, "TEST", string(body))
+}
