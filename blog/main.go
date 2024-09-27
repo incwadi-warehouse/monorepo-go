64b0fa6b79ff859449f8e6eaad9833f30882a166
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-09-27 20:22:04 +0000

    Remove default config

--- blog/main.go
+++ blog/main.go
@@ -5,12 +5,9 @@ import (
 
 	"github.com/incwadi-warehouse/monorepo-go/blog/router"
 	"github.com/incwadi-warehouse/monorepo-go/framework/config"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetDefault("CORS_ALLOW_ORIGIN", "http://127.0.0.1")
-
     config.LoadAppConfig()
 
 	r := router.Routes()
