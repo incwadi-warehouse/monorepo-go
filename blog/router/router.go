314e2f615993b1f0a9676271c31115e8fe601365
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-09-28 11:44:12 +0000

    Rrefactor router

--- blog/router/router.go
+++ blog/router/router.go
@@ -7,58 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/incwadi-warehouse/monorepo-go/blog/content/article"
 	"github.com/incwadi-warehouse/monorepo-go/blog/content/home"
-	"github.com/incwadi-warehouse/monorepo-go/framework/apikey"
-	"github.com/incwadi-warehouse/monorepo-go/framework/cors"
-	"github.com/spf13/viper"
+	"github.com/incwadi-warehouse/monorepo-go/framework/router"
 )
-
-// authMiddleware is a middleware to check for API key authentication.
-func authMiddleware(c *gin.Context) {
-	key := c.GetHeader("X-API-Key")
-
-	if !apikey.IsValidAPIKey(key) {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid API key"})
-		return
-	}
-
-	c.Next()
-}
-
-// permissionsMiddleware is a middleware to check for API key permissions.
-func permissionsMiddleware(permissions ...string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		key := c.GetHeader("X-API-Key")
-
-		for _, permission := range permissions {
-			if !apikey.HasPermission(key, permission) {
-				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Forbidden"})
-				return
-			}
-		}
-
-		c.Next()
-	}
-}
-
-func engine() *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-
-    corsConfig := cors.NewCors()
-    corsConfig.AllowOrigins = []string{viper.GetString("CORS_ALLOW_ORIGIN")}
-    corsConfig.SetCorsHeaders()
-	r.Use(corsConfig.SetCorsHeaders())
-
-    return r
-}
 
 // Routes sets up the Gin router.
 func Routes() *gin.Engine {
-	r := engine()
+	r := router.Engine()
 
-	api := r.Group("/", authMiddleware)
+	api := r.Group("/", router.ApiKeyMiddleware)
 	{
-		api.GET("/home", permissionsMiddleware("articles"), func(c *gin.Context) {
+		api.GET("/home", router.PermissionsMiddleware("articles"), func(c *gin.Context) {
 			index, err := home.GetHome()
 			if err != nil {
 				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -67,7 +25,7 @@ func Routes() *gin.Engine {
 
 			c.String(http.StatusOK, index)
 		})
-		api.GET("/home/new/:days", permissionsMiddleware("articles"), func(c *gin.Context) {
+		api.GET("/home/new/:days", router.PermissionsMiddleware("articles"), func(c *gin.Context) {
 			daysStr := c.Param("days")
 
 			days, err := strconv.Atoi(daysStr)
@@ -84,7 +42,7 @@ func Routes() *gin.Engine {
 
 			c.JSON(http.StatusOK, gin.H{"new_articles": newCount})
 		})
-		api.GET("/article/*path", permissionsMiddleware("articles"), func(c *gin.Context) {
+		api.GET("/article/*path", router.PermissionsMiddleware("articles"), func(c *gin.Context) {
 			path := c.Param("path")
 
 			cnt, err := article.GetArticle(path)
---
+++ framework/router/router.go
@@ -0,0 +1,53 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/incwadi-warehouse/monorepo-go/framework/apikey"
+	"github.com/incwadi-warehouse/monorepo-go/framework/cors"
+	"github.com/spf13/viper"
+)
+
+// ApiKeyMiddleware is a middleware to check for API key authentication.
+func ApiKeyMiddleware(c *gin.Context) {
+	key := c.GetHeader("X-API-Key")
+
+	if !apikey.IsValidAPIKey(key) {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid API key"})
+		return
+	}
+
+	c.Next()
+}
+
+// permissionsMiddleware is a middleware to check for API key permissions.
+func PermissionsMiddleware(permissions ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		key := c.GetHeader("X-API-Key")
+
+		for _, permission := range permissions {
+			if !apikey.HasPermission(key, permission) {
+				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Forbidden"})
+				return
+
+			}
+		}
+
+		c.Next()
+	}
+}
+
+// Engine creates a gin engine with CORS and sets it to release mode.
+func Engine() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.Default()
+
+    corsConfig := cors.NewCors()
+    corsConfig.AllowOrigins = []string{viper.GetString("CORS_ALLOW_ORIGIN")}
+    corsConfig.SetCorsHeaders()
+	r.Use(corsConfig.SetCorsHeaders())
+
+    return r
+}
